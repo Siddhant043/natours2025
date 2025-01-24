9912d732f2abe3c51d8772339b41b94792eb3acb
SiddhantMishra4 <sid@neuralix.ai>
2025-01-24 21:10:49 +0530

    16. added mongodb and mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,13 @@
 import "dotenv/config";
 import app from "./app.js";
+import mongoose from "mongoose";
+
+const password = process.env.DB_PASSWORD || "";
+const db = process.env.DB_URL?.replace("<db_password>", password) || "";
+
+mongoose.connect(db).then((con) => {
+  console.log("DB connection successfull");
+});
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
