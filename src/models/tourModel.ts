92c74b23ec2eb62e226941ac894b85b1e59d7b52
SiddhantMishra4 <sid@neuralix.ai>
2025-01-26 23:10:18 +0530

    34. Added Virtual properties

--- src/models/tourModel.ts
+++ src/models/tourModel.ts
@@ -1,62 +1,72 @@
 import mongoose, { Schema } from "mongoose";
 import { TourConfig } from "./types.js";
 
-const tourSchema: Schema<TourConfig> = new Schema({
-  name: {
-    type: String,
-    required: [true, "A Tour must have a name"],
-    unique: true,
+const tourSchema: Schema<TourConfig> = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "A Tour must have a name"],
+      unique: true,
+    },
+    duration: {
+      type: Number,
+      required: [true, "A Tour must have a duration"],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, "A Tour must have a group size"],
+    },
+    difficulty: {
+      type: String,
+      required: [true, "A Tour must have a difficulty"],
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: [true, "A tour must have a price"],
+    },
+    priceDiscount: {
+      type: Number,
+    },
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, "A tour must have a summary"],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, "A tour must have a image cover"],
+    },
+    images: {
+      type: [String],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    startDates: {
+      type: [Date],
+    },
   },
-  duration: {
-    type: Number,
-    required: [true, "A Tour must have a duration"],
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, "A Tour must have a group size"],
-  },
-  difficulty: {
-    type: String,
-    required: [true, "A Tour must have a difficulty"],
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 4.5,
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0,
-  },
-  price: {
-    type: Number,
-    required: [true, "A tour must have a price"],
-  },
-  priceDiscount: {
-    type: Number,
-  },
-  summary: {
-    type: String,
-    trim: true,
-    required: [true, "A tour must have a summary"],
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  imageCover: {
-    type: String,
-    required: [true, "A tour must have a image cover"],
-  },
-  images: {
-    type: [String],
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
-  startDates: {
-    type: [Date],
-  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+tourSchema.virtual("durationWeeks").get(function () {
+  return Number(this.duration) / 7;
 });
 
 const Tour = mongoose.model<TourConfig>("Tour", tourSchema);
