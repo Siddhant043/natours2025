ee199ed99d4d59e0bffbbd7563666a7f4e31f5a0
SiddhantMishra4 <sid@neuralix.ai>
2025-02-04 23:42:00 +0530

    40. implemented global err handling middleware

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Application, NextFunction, Request, Response } from "express";
 import morgan from "morgan";
 import tourRouter from "./routes/tourRoutes.js";
 import userRouter from "./routes/userRoutes.js";
+import { AppError } from "./types/errors.js";
 
 const app: Application = express();
 
@@ -19,9 +20,21 @@ app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
 app.all("*", (req, res, next) => {
-  res.status(404).json({
-    status: "failed",
-    message: `Can't find ${req.originalUrl} on the server!`,
+  const error: AppError = new Error(
+    `Can't find ${req.originalUrl} on the server!`
+  );
+  error.status = "fail";
+  error.statusCode = 404;
+  next(error);
+});
+
+app.use((err: AppError, req: Request, res: Response, next: NextFunction) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
