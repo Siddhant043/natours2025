0d68ad92f0ea3a7ec1c77b7252f04ac9f484108d
SiddhantMishra4 <sid@neuralix.ai>
2025-02-09 11:22:26 +0530

    43. Errors Segregated for dev and prod

--- src/controllers/errorController.ts
+++ src/controllers/errorController.ts
@@ -1,6 +1,30 @@
 import { NextFunction, Request, Response } from "express";
 import AppError from "../utils/appError.js";
 
+const sendErrorDev = (err: AppError, res: Response) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err: AppError, res: Response) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.error("ERROR 💥", err);
+    res.status(500).json({
+      status: "error",
+      message: "Something went wrong",
+    });
+  }
+};
+
 const globalErrorHandler = (
   err: AppError,
   req: Request,
@@ -10,9 +34,10 @@ const globalErrorHandler = (
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    sendErrorProd(err, res);
+  }
 };
 export default globalErrorHandler;
