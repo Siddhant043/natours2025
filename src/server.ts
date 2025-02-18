0b9b5fd94eedd3eb07230f44b2b82bf93add6e7e
SiddhantMishra4 <sid@neuralix.ai>
2025-02-19 01:30:39 +0530

    47. Catching uncaught exceptions

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,30 @@
 import "dotenv/config";
 import app from "./app.js";
-import mongoose from "mongoose";
+import mongoose, { Error } from "mongoose";
+
+process.on('uncaughtException', (err: Error) => {
+  console.log(err.name, err.message)
+  process.exit(1)
+})
 
 const password = process.env.DB_PASSWORD || "";
 const db = process.env.DB_URL?.replace("<db_password>", password) || "";
 
 mongoose.connect(db).then((con) => {
-  console.log("DB connection successfull");
+  console.log("DB connection successful");
 });
 
 const port = process.env.PORT || 8000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App is running on Port: ${port}`);
 });
+
+
+process.on('unhandledRejection', (err: Error) => {
+  console.log(err.name, err.message)
+  server.close(() => {
+    process.exit(1)
+  })
+})
+
+
