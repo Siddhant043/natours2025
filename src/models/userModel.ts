bfcd0f0d7fea246c46b235044052d6533c667b73
SiddhantMishra4 <sid@neuralix.ai>
2025-02-20 21:44:16 +0530

    50. User model setup with password encryption

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,6 +1,9 @@
 import { model, Schema } from "mongoose";
 import { UserConfig } from "./types.js";
 import pkg from 'validator';
+import bcrypt from "bcryptjs";
+
+
 const { isEmail } = pkg;
 
 const userSchema: Schema<UserConfig> = new Schema({
@@ -23,11 +26,32 @@ const userSchema: Schema<UserConfig> = new Schema({
         required: [true, "Password is a required field"],
         minlength: 8,
     },
-
     passwordConfirm: {
         type: String,
         required: [true, "Confirm Password is a required field"],
         minlength: 8,
+        validate: {
+            validator: function (el) {
+                return el === this.password
+            },
+            message: "Passwords are not the same"
+        }
+    }
+});
+
+userSchema.pre<UserConfig>('save', async function (next) {
+    if (!this.isModified('password')) return next();
+
+    try {
+        // Hash the password
+        this.password = await bcrypt.hash(this.password, 12);
+
+        // Set passwordConfirm to undefined
+        this.passwordConfirm = undefined;
+
+        next();
+    } catch (err) {
+        next(err as Error); // Pass the error to the next middleware
     }
 })
 
