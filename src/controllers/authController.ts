25cdfebbf3a4f537f6b6a6486b9aa46f86c796bb
Siddhant043 <siddhant48311@gmail.com>
2025-02-23 00:53:28 +0530

    52. jwt integrated for signup functionality

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express'
 import User from '../models/userModel.js'
 import catchAsync from '../utils/catchAsync.js'
+import jwt from 'jsonwebtoken'
 
 
 export const signup = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
@@ -10,10 +11,14 @@ export const signup = catchAsync(async (req: Request, res: Response, next: NextF
         password: req.body.password,
         passwordConfirm: req.body.passwordConfirm
     })
+    const jwtSecret = process.env.JWT_SECRET || "secret-should-be-at-least-thirty-two-characters-long"
+
+    const token = jwt.sign({ id: newUser._id }, jwtSecret, { expiresIn: "90d" })
 
 
     res.status(201).json({
         status: "success",
+        token,
         data: {
             user: newUser
         }
