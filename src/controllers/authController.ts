b3c03695f6e926bf921099cdcf667c18bc437398
Siddhant Mishra <siddhant48311@gmail.com>
2025-08-11 23:19:47 +0530

    Updated sending jwt method to using cookie

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -18,6 +18,31 @@ const signToken = (id: string) => {
   return token;
 };
 
+const sendToken = (res: Response, token:string, userData:any, ) => {
+
+  // Set cookie options
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + parseInt(process.env.JWT_COOKIE_EXPIRES_IN || "90") * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production", // Set to true if using HTTPS
+  };
+  // Set the token in a cookie
+  res.cookie("jwt", token, cookieOptions);
+  
+  // Remove password from output
+  userData.password = undefined;
+  userData.passwordConfirm = undefined;
+
+  res.status(201).json({
+      status: "success",
+      data: {
+        user: userData,
+      },
+    });
+}
+
 export const signup = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const newUser = await User.create({
@@ -29,13 +54,7 @@ export const signup = catchAsync(
 
     const token = signToken(newUser._id);
 
-    res.status(201).json({
-      status: "success",
-      token,
-      data: {
-        user: newUser,
-      },
-    });
+    
   }
 );
 
