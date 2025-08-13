8764056a14b5ba35c17cdf38105eff178b21651e
Siddhant Mishra <siddhant48311@gmail.com>
2025-08-13 23:41:20 +0530

    fixed errors with authcontroller

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -18,30 +18,34 @@ const signToken = (id: string) => {
   return token;
 };
 
-const sendToken = (res: Response, token:string, userData:any, ) => {
-
+const sendToken = (res: Response, token: string, userData: any) => {
   // Set cookie options
   const cookieOptions = {
     expires: new Date(
-      Date.now() + parseInt(process.env.JWT_COOKIE_EXPIRES_IN || "90") * 24 * 60 * 60 * 1000
+      Date.now() +
+        parseInt(process.env.JWT_COOKIE_EXPIRES_IN || "90") *
+          24 *
+          60 *
+          60 *
+          1000
     ),
     httpOnly: true,
     secure: process.env.NODE_ENV === "production", // Set to true if using HTTPS
   };
   // Set the token in a cookie
   res.cookie("jwt", token, cookieOptions);
-  
+
   // Remove password from output
   userData.password = undefined;
   userData.passwordConfirm = undefined;
 
   res.status(201).json({
-      status: "success",
-      data: {
-        user: userData,
-      },
-    });
-}
+    status: "success",
+    data: {
+      user: userData,
+    },
+  });
+};
 
 export const signup = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -53,8 +57,7 @@ export const signup = catchAsync(
     });
 
     const token = signToken(newUser._id);
-
-    
+    sendToken(res, token, newUser);
   }
 );
 
@@ -78,10 +81,7 @@ export const login = catchAsync(
 
     const token = signToken(user._id);
 
-    res.status(200).json({
-      status: "success",
-      token,
-    });
+    sendToken(res, token, user);
   }
 );
 
